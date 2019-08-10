f59205c7023f577fec184eff126f6be0bdeb5817
andreeaCcss <andreeaccss@gmail.com>
2019-08-10 14:30:22 +0200

    Steps 3 and 4

--- YOUR_JAVASCRIPT_HERE.js
+++ YOUR_JAVASCRIPT_HERE.js
@@ -9,12 +9,16 @@ const hero = {
         damage: 2
     }
 }
+const weapon = {
+    type: "",
+    damage: 2
+}
 
 function rest(hero) {
     if(hero.health == 10) {
-        alert("Health is already 10")
+        alert("Health is already 10");
     } else {
-        hero.health = 10
+        hero.health = 10;
     }
     return hero;
 }
@@ -23,8 +27,49 @@ function pickUpItem(hero, weapon) {
     weapon.type = "dagger";
     weapon.damage = 2;
     hero.inventory.push(weapon);
+    let image = document.getElementById("dagger");
+    image.style.display = "none";
 }
 
-function equipWeapon() {
-
+function equipWeapon(hero) {
+    if(hero.inventory.length === 0){
+        return null;
+    } else {
+        hero.weapon = hero.inventory[0];
+    }
+    displayStats();
 }
+
+function displayStats() {
+    let stats = document.getElementById("stats");
+    stats.textContent = `Hero name: ${hero.name}, Health: ${hero.health}, Weapon type: ${hero.weapon.type}, Weapon damage: ${hero.weapon.damage}`
+}
+
+function submitName() {
+    let inputField = document.getElementById("name");
+    let name = inputField.value;
+
+    if (doesNotPassAllValidations(name)) {
+        return null;
+    };
+    name = name.charAt(0).toUpperCase() + name.slice(1);
+
+    let header = document.getElementById("header");
+    header.textContent = `${name}`;
+    hero.name = name;
+    displayStats();
+
+    inputField.value = null;
+};
+
+
+function doesNotPassAllValidations (name) { 
+    if (!name) {
+        alert('You forgot to fill in the name!');
+        return true;
+    } else {
+        return false;
+    }
+}
+
+displayStats()
